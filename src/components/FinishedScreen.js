3ee5e117d5f88cd184496039aec5260545c9b83b
Souvik Sarkar <12345souvik12345@gmail.com>
2023-12-24 16:32:43 +0530

    initial commit

---
+++ src/components/FinishedScreen.js
@@ -0,0 +1,24 @@
+export default function FinishedScreen({
+  points,
+  maxPossiblePoints,
+  dispatch,
+}) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  return (
+    <>
+      <p className="result">
+        You Scored <strong>{points}</strong> out of {maxPossiblePoints}(
+        {Math.ceil(percentage)}%)
+      </p>
+      <button
+        className="btn btn-ui"
+        onClick={() => {
+          dispatch({ type: "restart" });
+        }}
+      >
+        Restart Quiz
+      </button>
+    </>
+  );
+}
